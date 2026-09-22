4f267e92eb55319ad2949869d7eade4c71cb4cf3
gethin101 <gethingrice@gmail.com>
2026-09-22 17:15:41 +0100

    Add Python script for city and landmark operations

---
+++ Subroutines/algorithm-to-python.py
@@ -0,0 +1,16 @@
+country = "United States of America"
+state = "California"
+city = "San Francisco"
+landmark = "Alcatraz Island"
+
+x = len(state)
+print(f"\nLength of state: {x}")
+
+print()
+print(city + " " + landmark)
+
+y = landmark[4:8]
+print(f"\nSubstring: {y}")
+
+z = landmark.find("t")
+print(f"\nPosition: {z}")
